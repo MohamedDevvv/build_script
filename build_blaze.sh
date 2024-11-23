fd129addb22decd82f70565f859ea09e4bd4fed6
MohamedDevvv <aaamohamed499@gmail.com>
2024-11-23 12:14:30 +0200

    Update build_blaze.sh

--- build_blaze.sh
+++ build_blaze.sh
@@ -5,11 +5,7 @@ rm -rf .repo/local_manifests
 #sync
 repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14
 git clone https://github.com/MohamedDevvv/Build- -b main .repo/local_manifests
-if [ -f /opt/crave/resync.sh ]; then
-  /opt/crave/resync.sh
-else
-  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-fi
+/opt/crave/resync.sh
 
 #build
 . build/envsetup.sh
